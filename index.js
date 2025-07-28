1201cd0a29e68c6081589413b4ff1fcb9472e19c
alejandraag-alt <alejandraag@somosziro.com>
2025-07-27 21:02:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
-const Tesseract = require('tesseract.js-node');
+const Tesseract = require('tesseract.js');
 
 const app = express();
 const port = process.env.PORT || 3000;
